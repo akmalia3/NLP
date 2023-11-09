8242561f87e4e081ad7c6c7facb705868a5509a4
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-09 14:53:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 st.header('Sentiment Analysis')
 st.subheader('Was the data helpful?')
 
-exel_file = "data-preprocessing.xlsx"
+excel_file = "data-preprocessing.xlsx"
 sheet_name = "sentiment-analysis"
 
 df = pd.read_excel(excel_file)
