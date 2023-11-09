e791826a46e7e499c3a42c20365336d5081a4632
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-09 15:17:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ df = df.drop(['Unnamed: 0'], axis=1)
 
 st.dataframe(df)
 
-# Visualisasi sumber data
-nilai = df['Sumber'].value_counts()
-chart = px.pie(values=nilai, names=['Twitter','Instagram'], title='Persentase Sentiment Sosial Media Dinas Kesehatan Kota Semarang', height=500, width=1000)
-chart.update_traces(textposition='auto', textinfo='percent+label', titleposition='bottom right')
-st.plotly_chart(chart)
+# Visualisasi
+kategori = df['Katagori'].value_counts()
+st.bar_chart(kategori, title='Kategori Pertanyaan')
