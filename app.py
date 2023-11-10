47817eb6998120d8e3e28ed72183b4e7efb8f11f
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-10 10:37:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ df = df.drop(['Unnamed: 0'], axis=1)
 
 st.dataframe(df)
 
-# Visualisasi Kategori
-kategori = df['Katagori'].value_counts()
-st.bar_chart(kategori)
-
 # Visualisasi sumber data
 sumber = df['Sumber'].value_counts()
 fig = px.pie(values=sumber, names=['Twitter','Instagram','X'], title="Persentase Sumber Data")
@@ -25,14 +21,18 @@ fig = px.pie(values=sumber, names=['Twitter','Instagram','X'], title="Persentase
 jenis_akun = df['Jenis Akun'].value_counts()
 fig_akun = px.pie(values=jenis_akun, names=['Asli','Fake'], title="Persentase Jenis Akun")
 
-left_column, right_column = st.columns(2)
-left_column.plotly_chart(fig, use_container_width=True)
-right_column.plotly_chart(fig_akun, use_container_width=True)
-
 # Visuaisasi jenis kelamin
 jenis_kelamin = df['Jenis Kelamin '].value_counts()
-fig_jk = px.pie(values=jenis_kelamin, names=['Laki-laki','Perempuan'])
-st.plotly_chart(fig_jk)
+fig_jk = px.pie(values=jenis_kelamin, names=['Laki-laki','Perempuan'], title="Persentase Jenis Kelamin User")
+
+left_column, right_column, midle_column = st.columns(3)
+left_column.plotly_chart(fig, use_container_width=True)
+right_column.plotly_chart(fig_akun, use_container_width=True)
+midle_column.st.plotly_chart(fig_jk)
+
+# Visualisasi Kategori
+kategori = df['Katagori'].value_counts()
+st.bar_chart(kategori)
 
 # Visualisasi tanggal komentar
 fig_tgl = px.area(df['Tanggal'])
