4c405f9bb605d552768be83c556ba80a11ae392f
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-10 10:54:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ jenis_akun = df['Jenis Akun'].value_counts()
 fig_akun = px.pie(values=jenis_akun, names=['Asli','Fake'], title="Persentase Jenis Akun")
 
 # Visuaisasi jenis kelamin
-jenis_kelamin = df['Jenis Kelamin'].value_counts()
+jenis_kelamin = df['Jenis Kelamin '].value_counts()
 fig_jk = px.pie(values=jenis_kelamin, names=['Laki-laki','Perempuan'], title="Persentase Jenis Kelamin User")
 
 left_column, right_column, midle_column = st.columns(3)
