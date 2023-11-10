3d74df488976109e3942464b601c2e94c9a93e82
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-10 11:47:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import matplotlib.pyplot as plt
+import wordcloud
 from wordcloud import WordCloud
 
 st.header('Sentiment Analysis')
