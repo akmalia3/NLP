fdae11157b29d881c155c54b32f6d9603fdc548c
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-13 13:05:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ data3 = pd.DataFrame(freq_neg.most_common(), columns=['word', 'frequent'])
 data3.style.background_gradient(cmap='Blues')
 
 neg_freq = px.bar(data3.head(30), x='frequent', y='word',
-            color='frequent', title="Top 30 Words Positive")
+            color='frequent', title="Top 30 Words Positive",  color="strength")
 neg_freq.update_layout(yaxis={'categoryorder':'total ascending'})
 st.plotly_chart(neg_freq, use_container_width=True)
 
