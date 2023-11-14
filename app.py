00a2d8454a354856d69a73b7b69eee2c14e2637d
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-14 09:29:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,21 @@ fig_freq = px.bar(data.head(40), x='frequent', y='word',
 fig_freq.update_layout(yaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig_freq, use_container_width=True)
 
+# wordcloud
+wordcloud = WordCloud(width = 2000, height = 1334,
+                      random_state=1, background_color='black',colormap='Pastel1',
+                      collocations=False, normalize_plurals=False,
+                      collocation_threshold = 2).generate(ngram)
+#visualisasi
+plt.figure()
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+left, right = st.columns(2)
+left.plotly_chart(fig_freq, use_container_width=True)
+right.pyplot(plt, use_container_width=True)
+
 # frequent bigrams
 df['bigrams'].fillna(' ', inplace=True)
 bigram = ''.join(df['bigrams'])
@@ -124,18 +139,6 @@ neg_freq = px.bar(data3.head(30), x='frequent', y='word', title="Top 30 Words Ne
                  color_discrete_sequence= px.colors.sequential.Plasma_r, color='frequent')
 neg_freq.update_layout(yaxis={'categoryorder':'total ascending'})
 st.plotly_chart(neg_freq, use_container_width=True)
-
-# wordcloud
-wordcloud = WordCloud(width = 2000, height = 1334,
-                      random_state=1, background_color='black',colormap='Pastel1',
-                      collocations=False, normalize_plurals=False,
-                      collocation_threshold = 2).generate(ngram)
-#visualisasi
-plt.figure()
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-st.pyplot(plt)
 
 # wordcloud bigrams
 wordcloud_bigrams = WordCloud(width = 2000, height = 1334,
