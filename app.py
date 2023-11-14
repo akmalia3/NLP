6da5e57eed6ef9a44e608156b9cb289733126df3
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-14 11:37:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ plt.axis("off")
 plt.show()
 
 # Display left and right side
-left, right = st.columns([1,2,1],gap='large')
+left, right = st.columns([1,1],gap='large')
 left.subheader('wordcloud')
 left.pyplot(plt)
 right.subheader('frequent word')
