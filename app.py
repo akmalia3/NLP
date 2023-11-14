c63ebcc373412524f1f4cf7fa60d124fcdc17cba
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-14 13:55:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from collections import Counter
 
 st.header('Sentiment Analysis')
 st.subheader('Was the data helpful?')
+st.set_page_config(layout="wide")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
 excel_file = "dataset.xlsx"
 df = pd.read_excel(excel_file)
