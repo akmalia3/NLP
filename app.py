d91c974c9a2ad0d105b63ae0bde66205927f82e6
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-14 21:01:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ df_selection = df.query("Sumber == @sumber_data")
 col1, col2 = st.columns(2)
 
 with col1:
+    st.markdown('<div style="border: 1px solid #e6e6e6; padding: 10px; border-radius: 5px;">', unsafe_allow_html=True)
     st.markdown('Data')
     st.dataframe(df_selection)
 
