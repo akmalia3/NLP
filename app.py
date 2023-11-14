98be0615b4b5a7c660b2bc96d31285a653a0da34
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-14 11:52:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ fig_sentiment = px.pie(values=sentiment, names=['positive','negative'], title='P
 st.plotly_chart(fig_sentiment)
 
 # frequents word
+'''
 df['ngrams'].fillna(' ', inplace=True)
 ngram = ''.join(df['ngrams'])
 
@@ -77,13 +78,37 @@ plt.figure(figsize=(30,30))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
-
+'''
 # Display left and right side
-left, right = st.columns(2, gap='large')
-left.subheader('wordcloud')
-left.pyplot(plt, use_container_width=True)
-right.subheader('frequent word')
-right.plotly_chart(fig_freq, use_container_width=True)
+left, right = st.columns(2)
+
+with left:
+            wordcloud = WordCloud(width = 2000, height = 2000,
+                      random_state=1, background_color='white',colormap='PuBu_r',
+                      collocations=False, normalize_plurals=False,
+                      collocation_threshold = 2).generate(ngram)
+#visualisasi
+            plt.figure(figsize=(30,30))
+            plt.imshow(wordcloud, interpolation='bilinear')
+            plt.axis("off")
+            plt.show()
+            st.subheader('wordcloud')
+            st.pyplot(plt, use_container_width=True)
+
+with right:
+            df['ngrams'].fillna(' ', inplace=True)
+            ngram = ''.join(df['ngrams'])
+
+            text = ngram.split()
+            freq = Counter(text)
+            data = pd.DataFrame(freq.most_common(), columns=['word', 'frequent'])
+            data.style.background_gradient(cmap='Blues')
+
+            fig_freq = px.bar(data.head(40), x='frequent', y='word', color='frequent')
+            fig_freq.update_layout(yaxis={'categoryorder':'total ascending'})
+            st.plotly_chart(fig_freq, use_container_width=True)
+            st.subheader('frequent word')
+
 
 # frequent bigrams
 df['bigrams'].fillna(' ', inplace=True)
