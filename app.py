80936639f4cd1b8f66b1ad4f773a7d70db0becbb
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-17 10:56:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ with right:
         sumber_data = st.selectbox("Pilih Sumber Data", options=df["Sumber"].unique())
         
     with nav2:
-        sentiment = st.selectbox("Pilih Sentiment", options=df["Sentiment"].unique())
+        sentiment = st.selectbox("Pilih Sentiment", options=df["sentiment"].unique())
         
     with nav3:
         tahun = st.selectbox("Pilih Bulan", options=df["Sumber"].unique())
