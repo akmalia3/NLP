4d09be23bfe22082adfce5d638fab3ba0e69b91c
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-20 13:35:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,8 @@ with nav5:
     data = pd.to_datetime(df['Tanggal'], format="%Y-%m-%d", errors='coerce').dt.tz_localize(None)
     start = data.min()
     finish = data.max()
-    start_date, end_date = st.sidebar.date_input(label='Rentang Waktu',
-                                        min_value=start,
-                                        max_value=finish,
-                                        value=[min_value, max_value])
+    start_date, end_date = st.sidebar.date_input(label='Rentang Waktu', start, finish,
+                                                 value=[start, finish])
     #tanggal = st.date_input("Pilih tanggal",
                     #(start, finish),
                     #start,
